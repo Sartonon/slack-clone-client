db8f25537af3c6bb2aeca00c510fe3f447927bdf
Santeri Rusila <santtu_1993@hotmail.com>
2018-05-07 23:37:36 +0300

    Initial client

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  /*"plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],*/
+  rules: {
+    "react/jsx-filename-extension": 0
+  },
+  "globals": {
+    "document": 1
+  }
+};
