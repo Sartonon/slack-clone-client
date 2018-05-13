c377ecdd3f72582281d50fb2ceab0fcbfd56be71
Santeri Rusila <santtu_1993@hotmail.com>
2018-05-13 16:37:22 +0300

    Container component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  extends: ['airbnb', 'eslint:recommended', 'plugin:react/recommended'],
-  /*"plugins": [
-    "react",
-    "jsx-a11y",
-    "import"
-  ],*/
+  extends: ['airbnb', 'prettier', 'eslint:recommended', 'plugin:react/recommended'],
   rules: {
     'react/jsx-filename-extension': 0,
     'react/prop-types': 0,
